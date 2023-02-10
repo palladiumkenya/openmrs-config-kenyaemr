5dc211a02988fa6203306dd92dbc011adea04d39
Donald Kibet <chelashawdonald@yahoo.com>
2023-02-11 00:20:06 +0300

    feat: add bash build command

---
+++ build.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo "Building Kenya EMR 3.x assests ..."
+
+rm -rf openmrs-config-kenyaemr
+rm -rf spa
+
+CWD=$(pwd)
+
+echo 
+
+npx --legacy-peer-deps openmrs@next build --build-config ./configuration/spa-build-config.json --target ./spa --page-title KenyaEMR
+npx --legacy-peer-deps openmrs@next assemble --manifest --mode config --config ./configuration/spa-build-config.json --target ./spa
+
+git clone https://github.com/palladiumkenya/openmrs-config-kenyaemr.git
+
+# remove OpenMRS favicon
+
+rm -rf ${CWD}/assets/favicon.ico
+
+cp ${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-login-logo.png ${CWD}/spa
+cp ${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-primary-logo.png ${CWD}/spa
+cp ${CWD}/assets/favicon.ico ${CWD}/spa
+cp ${CWD}/configuration/config.json ${CWD}/spa
