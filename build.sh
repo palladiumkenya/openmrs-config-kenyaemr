5d356432cf7897c27a5955d50a67bb05088497b2
Donald Kibet <chelashawdonald@yahoo.com>
2023-04-04 22:33:58 +0300

    (chore) bump build command and update config (#18)

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ CWD=$(pwd)
 
 echo 
 
-npx --legacy-peer-deps openmrs@next build --build-config ./configuration/spa-build-config.json --target ./spa --page-title KenyaEMR
+npx --legacy-peer-deps openmrs@next build --build-config ./configuration/spa-build-config.json --target ./spa --page-title KenyaEMR  --support-offline false
 npx --legacy-peer-deps openmrs@next assemble --manifest --mode config --config ./configuration/spa-build-config.json --target ./spa
 
 git clone https://github.com/palladiumkenya/openmrs-config-kenyaemr.git
@@ -20,5 +20,5 @@ rm -rf ${CWD}/assets/favicon.ico
 
 cp ${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-login-logo.png ${CWD}/spa
 cp ${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-primary-logo.png ${CWD}/spa
-cp ${CWD}/assets/favicon.ico ${CWD}/spa
+cp ${CWD}/openmrs-config-kenyaemr/assets/favicon.ico ${CWD}/spa
 cp ${CWD}/configuration/config.json ${CWD}/spa
