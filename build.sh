988845f68886ce7f95e9889262821da1a6256fe0
Donald Kibet <chelashawdonald@yahoo.com>
2023-04-11 10:27:24 +0300

    (chore) update the build shell script (#22)

--- build.sh
+++ build.sh
@@ -1,24 +1,34 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "Building Kenya EMR 3.x assests ..."
-
+# Clean up previous build artifacts
+echo "Cleaning up previous build artifacts ..."
 rm -rf openmrs-config-kenyaemr
 rm -rf spa
 
+# Build assets
+echo "Building Kenya EMR 3.x assets ..."
 CWD=$(pwd)
+npx --legacy-peer-deps openmrs@next build \
+  --build-config ./configuration/spa-build-config.json \
+  --target ./spa \
+  --page-title "KenyaEMR" \
+  --support-offline false
 
-echo 
+# Assemble assets
+echo "Assembling assets ..."
+npx --legacy-peer-deps openmrs@next assemble \
+  --manifest \
+  --mode config \
+  --config ./configuration/spa-build-config.json \
+  --target ./spa
 
-npx --legacy-peer-deps openmrs@next build --build-config ./configuration/spa-build-config.json --target ./spa --page-title KenyaEMR  --support-offline false
-npx --legacy-peer-deps openmrs@next assemble --manifest --mode config --config ./configuration/spa-build-config.json --target ./spa
-
+# Copy required files
+echo "Copying required files ..."
 git clone https://github.com/palladiumkenya/openmrs-config-kenyaemr.git
+cp "${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-login-logo.png" "${CWD}/spa"
+cp "${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-primary-logo.png" "${CWD}/spa"
+cp "${CWD}/openmrs-config-kenyaemr/assets/favicon.ico" "${CWD}/spa"
+cp "${CWD}/configuration/config.json" "${CWD}/spa"
 
-# remove OpenMRS favicon
-
-rm -rf ${CWD}/assets/favicon.ico
-
-cp ${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-login-logo.png ${CWD}/spa
-cp ${CWD}/openmrs-config-kenyaemr/assets/kenyaemr-primary-logo.png ${CWD}/spa
-cp ${CWD}/openmrs-config-kenyaemr/assets/favicon.ico ${CWD}/spa
-cp ${CWD}/configuration/config.json ${CWD}/spa
+# Exit with success status
+exit 0
