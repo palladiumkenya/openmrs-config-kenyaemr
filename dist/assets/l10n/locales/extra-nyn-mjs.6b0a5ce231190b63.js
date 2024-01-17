c852644d2be18575708c1e2e1f66d8dd865eaf53
Donald Kibet <chelashawdonald@yahoo.com>
2024-01-17 16:32:26 +0300

    (chore) bumb form engine and patient chart assests to support custom work

--- dist/assets/l10n/locales/extra-nyn-mjs.6b0a5ce231190b63.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_openmrs_esm_form_entry_app=self.webpackChunk_openmrs_esm_form_entry_app||[]).push([[86662],{40626:(s,_,e)=>{e.r(_),e.d(_,{default:()=>n});const n=[]}}]);
