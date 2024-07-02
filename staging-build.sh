6a1acb1ea588318b82f99e09b512bc29329b0ef5
Donald Kibet <chelashawdonald@yahoo.com>
2024-07-02 14:17:53 +0300

    Update staging-build.sh

--- staging-build.sh
+++ staging-build.sh
@@ -59,7 +59,6 @@ rename_dist_folder() {
 
 # Handle renaming for openmrs-esm-form-entry-app-*
 rename_dist_folder "openmrs-esm-form-entry-app-*" "dist-form-entry"
-rename_dist_folder "@openmrs/esm-service-queues-app-*" "dist-service-queues"
 
 
 # Exit with success status
