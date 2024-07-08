06535a99d07aded8622c2d3704c7d4d001233913
Donald Kibet <chelashawdonald@yahoo.com>
2024-07-08 15:26:08 +0300

    (pre-release) update staging build-config

--- staging-build.sh
+++ staging-build.sh
@@ -8,7 +8,7 @@ rm -rf frontend
 # Build assets
 echo "Building Kenya EMR 3.x assets ..."
 CWD=$(pwd)
-npx --legacy-peer-deps openmrs@5.6.1-pre.1996 build \
+npx --legacy-peer-deps openmrs@5.6.1-pre.2043 build \
   --build-config ./configuration/staging-build-config.json \
   --target ./frontend \
   --page-title "KenyaEMR" \
@@ -16,7 +16,7 @@ npx --legacy-peer-deps openmrs@5.6.1-pre.1996 build \
 
 # Assemble assets
 echo "Assembling assets ..."
-npx --legacy-peer-deps openmrs@5.6.1-pre.1996 assemble \
+npx --legacy-peer-deps openmrs@5.6.1-pre.2043 assemble \
   --manifest \
   --mode config \
   --config ./configuration/staging-build-config.json \
