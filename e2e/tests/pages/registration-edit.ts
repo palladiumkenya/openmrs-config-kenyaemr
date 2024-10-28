d94848b52bfea2643e713f835a131f7de86529e8
Joshua Murithi <brodiamyzalius@gmail.com>
2024-10-29 02:24:40 +0300

    (OPS): adding dev-deployment workflows
    Signed-off-by: Joshua Murithi <brodiamyzalius@gmail.com>

--- e2e/tests/pages/registration-edit.ts
+++ e2e/tests/pages/registration-edit.ts
@@ -1,81 +1,120 @@
-import { type Locator, type Page, expect } from '@playwright/test';
+import {type Locator, type Page, expect} from '@playwright/test';
 
 export type PatientRegistrationSex = 'male' | 'female' | 'other' | 'unknown';
+export type MaritalStatus =
+    "Married polygamous"
+    | "Married monogamous"
+    | "Divorced"
+    | "Widowed"
+    | "Cohabiting"
+    | "Single"
+export type Occupation = "Farmer" | "Employee" | "Trader" | "Student" | "Driver" | "None" | "Other"
+export type Education =
+    "None"
+    | "Primary School education"
+    | "Secondary school education"
+    | "College, university or polytechnic"
 
 export interface PatientRegistrationFormValues {
-  givenName?: string;
-  middleName?: string;
-  familyName?: string;
-  sex?: PatientRegistrationSex;
-  birthdate?: {
-    day: string;
-    month: string;
-    year: string;
-  };
-  postalCode?: string;
-  address1?: string;
-  address2?: string;
-  country?: string;
-  countyDistrict?: string;
-  stateProvince?: string;
-  cityVillage?: string;
-  phone?: string;
-  email?: string;
+    givenName?: string;
+    middleName?: string;
+    familyName?: string;
+    sex?: PatientRegistrationSex;
+    birthdate?: {
+        day: string;
+        month: string;
+        year: string;
+    };
+    county?: string,
+    subCounty?: string,
+    ward?: string,
+    nationalID?: string,
+    phone?: string,
+    postal_address?: string,
+    email?: string,
+    location?: string,
+    sublocation?: string,
+    village?: string,
+    landMark?: string,
+    healthCentre?: string,
+    maritalStatus?: string,
+    occupation?: string,
+    education?: string,
+    nextOfkinName?: string,
+    nextOfKinRelationship?: string,
+    nextOfKinNumber?: string,
+    nextOfkinPostalAddres?: string,
 }
 
 export class RegistrationAndEditPage {
-  constructor(readonly page: Page) {}
-  async gotoRegistration() {
-    await this.page.goto('/openmrs/spa/patient-registration');
-  }
+    constructor(readonly page: Page) {
+    }
 
-  readonly givenNameInput = () => this.page.locator('#givenName');
-  readonly middleNameInput = () => this.page.locator('#middleName');
-  readonly familyNameInput = () => this.page.locator('#familyName');
-  readonly sexRadioButton = (sex: PatientRegistrationSex) => this.page.locator(`label[for=gender-option-${sex}]`);
-  readonly birthDateInput = () => this.page.locator('#birthdate');
-  readonly birthdateDayInput = () => this.birthDateInput().locator('[data-type="day"]');
-  readonly birthdateMonthInput = () => this.birthDateInput().locator('[data-type="month"]');
-  readonly birthdateYearInput = () => this.birthDateInput().locator('[data-type="year"]');
-  readonly countyInput = () => this.page.locator('#countyDistrict');
-  readonly stateCountyInput = () => this.page.locator('#stateProvince');
-  readonly cityVillageInput = () => this.page.locator('#cityVillage');
-  readonly wardInput = () => this.page.locator("#address4");
-  readonly phoneInput = () => this.page.locator('#phone');
-  readonly emailInput = () => this.page.locator('#email');
-  readonly locationInput =()=> this.page.locator("#address.address6");
-  readonly subLocationInput =()=>this.page.locator("#address.address5");
-  readonly postalAddressInput =()=>this.page.locator("#address.address1");
-  readonly villageInput=()=>this.page.locator("#address.cityVillage");
-  readonly landmarkInput =()=> this.page.locator("#address.address2");
-  readonly nearestHealthCentre=()=> this.page.locator("#healthCenter");
-  readonly createPatientButton = () => this.page.locator('button[type=submit]');
+    async gotoRegistration() {
+        await this.page.goto('/patient-registration');
+    }
 
-  async goto(editPatientUuid?: string) {
-    await this.page.goto(editPatientUuid ? `patient/${editPatientUuid}/edit` : 'patient-registration');
-  }
+    readonly givenNameInput = () => this.page.locator('#givenName');
+    readonly middleNameInput = () => this.page.locator('#middleName');
+    readonly familyNameInput = () => this.page.locator('#familyName');
+    readonly sexRadioButton = (sex: PatientRegistrationSex) => this.page.locator(`label[for=gender-option-${sex}]`);
+    readonly birthDateInput = () => this.page.locator('#birthdate');
+    readonly birthdateDayInput = () => this.birthDateInput().locator('[data-type="day"]');
+    readonly birthdateMonthInput = () => this.birthDateInput().locator('[data-type="month"]');
+    readonly birthdateYearInput = () => this.birthDateInput().locator('[data-type="year"]');
+    readonly countyInput = () => this.page.locator('#countyDistrict');
+    readonly stateCountyInput = () => this.page.locator('#stateProvince');
+    readonly cityVillageInput = () => this.page.locator('#cityVillage');
+    readonly wardInput = () => this.page.locator("#address4");
+    readonly phoneInput = () => this.page.locator('#phone');
+    readonly emailInput = () => this.page.locator('#email');
+    readonly locationInput = () => this.page.getByLabel('Location', {exact: true});
+    readonly subLocationInput = () => this.page.getByLabel('Sub-location', {exact: true});
+    readonly postalAddressInput = () => this.page.getByLabel('Contact Details Section').getByLabel('Postal Address (optional)');
+    readonly villageInput = () => this.page.getByLabel('Village');
+    readonly landmarkInput = () => this.page.getByLabel('Landmark', {exact: true});
+    readonly nearestHealthCentre = () => this.page.getByLabel('Nearest Health Center');
+    readonly maritalStatus = (status: string | undefined) => this.page.getByLabel('Marital status').selectOption('5555AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    readonly occupationalInput = (occupation: string | undefined) => this.page.getByLabel('Occupation').selectOption('1540AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    readonly educationInput = (education: string | undefined) => this.page.getByLabel('Education').selectOption('1713AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+    readonly postToRegistryButton = () => this.page.getByRole('button', {name: 'Post to registry'});
+    readonly createPatientButton = () => this.page.locator('button[type=submit]');
 
-  async waitUntilTheFormIsLoaded() {
-    await expect(this.createPatientButton()).toBeDisabled();
-  }
+    async goto(editPatientUuid?: string) {
+        await this.page.goto(editPatientUuid ? `patient/${editPatientUuid}/edit` : 'patient-registration');
+    }
 
-  async fillPatientRegistrationForm(formValues: PatientRegistrationFormValues) {
-    const tryFill = (locator: Locator, value?: string) => value && locator.fill(value);
-    await tryFill(this.givenNameInput(), formValues.givenName);
-    await tryFill(this.middleNameInput(), formValues.middleName);
-    await tryFill(this.familyNameInput(), formValues.familyName);
-    formValues.sex && (await this.sexRadioButton(formValues.sex).check());
-    this.birthDateInput().getByRole('presentation').focus();
-    await tryFill(this.birthdateDayInput(), formValues.birthdate.day);
-    await tryFill(this.birthdateMonthInput(), formValues.birthdate.month);
-    await tryFill(this.birthdateYearInput(), formValues.birthdate.year);
-    await tryFill(this.phoneInput(), formValues.phone);
-    await tryFill(this.emailInput(), formValues.email);
-    await tryFill(this.address1Input(), formValues.address1);
-    await tryFill(this.stateProvinceInput(), formValues.stateProvince);
-    await tryFill(this.cityVillageInput(), formValues.cityVillage);
-    await tryFill(this.countryInput(), formValues.country);
-    await tryFill(this.countyDistrictInput(), formValues.countyDistrict);
-    await this.createPatientButton().click();
-  }
+    // clickin the post-to registry as one confirms the submit button is also disabled
+    async waitUntilTheFormIsLoaded() {
+        await expect(this.createPatientButton()).toBeDisabled();
+        await this.postToRegistryButton().click();
+        await expect(this.createPatientButton()).toBeVisible();
+    }
+
+    async fillPatientRegistrationForm(formValues: PatientRegistrationFormValues) {
+        const tryFill = (locator: Locator, value?: string) => value && locator.fill(value);
+        await tryFill(this.givenNameInput(), formValues.givenName);
+        await tryFill(this.middleNameInput(), formValues.middleName);
+        await tryFill(this.familyNameInput(), formValues.familyName);
+        formValues.sex && (await this.sexRadioButton(formValues.sex).check());
+        this.birthDateInput().getByRole('presentation').focus();
+        await tryFill(this.birthdateDayInput(), formValues.birthdate.day);
+        await tryFill(this.birthdateMonthInput(), formValues.birthdate.month);
+        await tryFill(this.birthdateYearInput(), formValues.birthdate.year);
+        await tryFill(this.countyInput(), formValues.county);
+        await tryFill(this.stateCountyInput(), formValues.subCounty);
+        await tryFill(this.wardInput(), formValues.ward);
+        await tryFill(this.phoneInput(), formValues.phone);
+        await tryFill(this.emailInput(), formValues.email);
+        await tryFill(this.locationInput(), formValues.location);
+        await tryFill(this.subLocationInput(), formValues.sublocation);
+        await tryFill(this.postalAddressInput(), formValues.postal_address);
+        await tryFill(this.villageInput(), formValues.village);
+        await tryFill(this.landmarkInput(), formValues.landMark);
+        await tryFill(this.nearestHealthCentre(), formValues.healthCentre);
+        await this.educationInput(formValues.education);
+        await this.occupationalInput(formValues.occupation);
+        await this.maritalStatus(formValues.maritalStatus)
+        await this.createPatientButton().click();
+    }
 }
--- e2e/tests/specs/registration/registration-test.spec.ts
+++ e2e/tests/specs/registration/registration-test.spec.ts
@@ -1,149 +1,177 @@
-import { expect } from '@playwright/test';
-import { test } from '../../core';
-import { type PatientRegistrationFormValues, RegistrationAndEditPage } from '../../pages';
-import { deletePatient } from '../../commands';
+import {expect} from '@playwright/test';
+import {test} from '../../core';
+import {type PatientRegistrationFormValues, RegistrationAndEditPage} from '../../pages';
+import {deletePatient} from '../../commands';
 
 let patientUuid: string;
 
-test('Register a new patient', async ({ page }) => {
-  test.setTimeout(5 * 60 * 1000);
-  const patientRegistrationPage = new RegistrationAndEditPage(page);
+test('Register a new patient', async ({page}) => {
+    test.setTimeout(5 * 60 * 1000);
+    const patientRegistrationPage = new RegistrationAndEditPage(page);
 
-  const formValues: PatientRegistrationFormValues = {
-    givenName: 'James',
-    middleName: 'Doe',
-    familyName: 'Test',
-    nationalId:'',
-    sex: 'male',
-    birthdate: { day: '01', month: '02', year: '1990' },
-    country: 'Kenya',
-    subCounty: 'Pernambuco',
-    ward: 'Recife',
-    phone: '5555551234',
-    postal_address: '123 10230',
-    email:"test@email.com",
-    location:'',
-    sublocation:'',
-    village:'',
-    landMark:'test landmark',
-    healthCentre: '',
-    maritalStatus:'',
-    euducation:'',
-    nextOfkinName:'',
-    nextOfKinRelationship:'',
-    nextOfKinNumber:'',
-    nextOfkinPostalAddres:'',
+    const formValues: PatientRegistrationFormValues = {
+        givenName: 'James',
+        middleName: 'Doe',
+        familyName: 'Test',
+        nationalID: '34089324',
+        sex: 'male',
+        birthdate: {day: '01', month: '02', year: '1990'},
+        county: 'Baringo',
+        subCounty: 'BaringoNorth',
+        ward: 'Barwessa',
+        phone: '0755551234',
+        postal_address: '12310230',
+        email: "test@email.com",
+        location: 'testLocation',
+        sublocation: 'testSublocation',
+        village: 'testVillage',
+        landMark: 'testLandmark',
+        healthCentre: 'testHealthcentre',
+        maritalStatus: '159715AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        occupation: '1540AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        education: "1714AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        nextOfkinName: 'test nextkin',
+        nextOfKinRelationship: '',
+        nextOfKinNumber: '0700000000',
+        nextOfkinPostalAddres: '',
 
-  };
+    };
 
-  await test.step('When I visit the registration page', async () => {
-    await patientRegistrationPage.goto();
-    await patientRegistrationPage.waitUntilTheFormIsLoaded();
-    await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/patient-registration`);
-  });
+    await test.step('When I visit the registration page', async () => {
+        await patientRegistrationPage.goto();
+        await patientRegistrationPage.waitUntilTheFormIsLoaded();
+        await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/patient-registration`);
+    });
+    await test.step("then I should be able to view post to registry", async () => {
+        await page.getByRole('heading', {name: 'Client verification with'}).isVisible()
+    })
+    await test.step('And then I fill the registration form with the data in `formValues` and then click the `Submit` button', async () => {
+        await patientRegistrationPage.fillPatientRegistrationForm(formValues);
+    });
 
-  await test.step('And then I fill the registration form with the data in `formValues` and then click the `Submit` button', async () => {
-    await patientRegistrationPage.fillPatientRegistrationForm(formValues);
-  });
+    await test.step('Then I should see a success notification', async () => {
+        await expect(page.getByText(/new patient created/i)).toBeVisible();
+    });
 
-  await test.step('Then I should see a success notification', async () => {
-    await expect(page.getByText(/new patient created/i)).toBeVisible();
-  });
+    await test.step("And I should be redirected to the new patient's chart page", async () => {
+        const patientChartUrlRegex = new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$');
+        await page.waitForURL(patientChartUrlRegex);
+        await expect(page).toHaveURL(patientChartUrlRegex);
+    });
 
-  await test.step("And I should be redirected to the new patient's chart page", async () => {
-    const patientChartUrlRegex = new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$');
-    await page.waitForURL(patientChartUrlRegex);
-    await expect(page).toHaveURL(patientChartUrlRegex);
-  });
+    await test.step("And I should see the newly registered patient's details displayed in the patient banner", async () => {
+        const patientBanner = page.locator('header[aria-label="patient banner"]');
 
-  await test.step("And I should see the newly registered patient's details displayed in the patient banner", async () => {
-    const patientBanner = page.locator('header[aria-label="patient banner"]');
+        await expect(patientBanner).toBeVisible();
+        await expect(patientBanner.getByText('James Doe Test')).toBeVisible();
+        await expect(patientBanner.getByText(/male/i)).toBeVisible();
+        await expect(patientBanner.getByText(/01 — Feb — 1990/i)).toBeVisible();
+        await expect(patientBanner.getByText(/OpenMRS ID/i)).toBeVisible();
+    });
 
-    await expect(patientBanner).toBeVisible();
-    await expect(patientBanner.getByText('Johnny Donny Ronny')).toBeVisible();
-    await expect(patientBanner.getByText(/male/i)).toBeVisible();
-    await expect(patientBanner.getByText(/01 — Feb — 2020/i)).toBeVisible();
-    await expect(patientBanner.getByText(/OpenMRS ID/i)).toBeVisible();
-  });
+    await test.step('And when I click the `Show details` button in the patient banner', async () => {
+        await page
+            .getByLabel('patient banner')
+            .getByRole('button', {name: /show details/i})
+            .click();
+    });
 
-  await test.step('And when I click the `Show details` button in the patient banner', async () => {
-    await page
-      .getByLabel('patient banner')
-      .getByRole('button', { name: /show details/i })
-      .click();
-  });
+    await test.step("Then I should see the patient's address and contact details displayed in the patient banner", async () => {
+        const patientBanner = page.locator('header[aria-label="patient banner"]');
 
-  await test.step("Then I should see the patient's address and contact details displayed in the patient banner", async () => {
-    const patientBanner = page.locator('header[aria-label="patient banner"]');
-
-    await expect(patientBanner.getByRole('button', { name: /hide details/i })).toBeVisible();
-    await expect(patientBanner.getByText(/^address$/i)).toBeVisible();
-    await expect(patientBanner.getByText(/address line 1: Bom Jesus Street/i)).toBeVisible();
-    await expect(patientBanner.getByText(/city: Recife/i)).toBeVisible();
-    await expect(patientBanner.getByText(/state: Pernambuco/i)).toBeVisible();
-    await expect(patientBanner.getByText(/country: Brazil/i)).toBeVisible();
-    await expect(patientBanner.getByText(/contact details/i)).toBeVisible();
-    await expect(patientBanner.getByText(/telephone number: 5555551234/i)).toBeVisible();
-  });
+        await expect(patientBanner.getByRole('button', {name: /hide details/i})).toBeVisible();
+        await expect(patientBanner.getByText(/^address$/i)).toBeVisible();
+        await expect(patientBanner.getByText(/address line 1: 12310230/i)).toBeVisible();
+        await expect(patientBanner.getByText(/city: testVillage/i)).toBeVisible();
+        await expect(patientBanner.getByText(/state: BaringoNorth/i)).toBeVisible();
+        await expect(patientBanner.getByText(/contact details/i)).toBeVisible();
+        await expect(patientBanner.getByText(/Telephone contact: 0755551234/i)).toBeVisible();
+    });
 });
 
-test('Register an unknown patient', async ({ api, page }) => {
-  const patientRegistrationPage = new RegistrationAndEditPage(page);
 
-  await test.step('When I visit the patient registration page', async () => {
-    await patientRegistrationPage.goto();
-    await patientRegistrationPage.waitUntilTheFormIsLoaded();
-  });
+test('Registering Client with unkown Birth date', async ({page}) => {
+    await test.step('When I visit the registration page', async () => {
+        const patientRegistrationPage = new RegistrationAndEditPage(page)
+        await patientRegistrationPage.goto();
+        await patientRegistrationPage.waitUntilTheFormIsLoaded();
+        await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/patient-registration`);
+    });
+    await test.step("Entering Basic info", async () => {
+        await page.getByLabel('First Name').click();
+        await page.getByLabel('First Name').fill('James');
+        await page.getByLabel('Middle Name (optional)').click();
+        await page.getByLabel('Middle Name (optional)').fill('Deivin');
+        await page.getByLabel('Family Name').click();
+        await page.getByLabel('Family Name').fill('DOE');
+        await page.getByText('Female').click();
+        await page.getByRole('tab', {name: 'No'}).click();
+        await page.getByLabel('Estimated age in years').click();
+        await page.getByLabel('Estimated age in years').fill('24');
+        await page.getByLabel('Estimated age in months').click();
+        await page.getByLabel('Estimated age in months').fill('1');
+        await page.getByLabel('National ID (optional)').click();
+        await page.getByLabel('National ID (optional)').fill('34059833');
+    });
+    await test.step("Entering Contact and demographics data", async () => {
+        await page.getByPlaceholder('Search address').click();
+        await page.getByPlaceholder('Search address').fill('ba');
+        await page.getByText('Homa bay > Homa Bay Town > Homa Bay West').click();
+        await page.getByLabel('Telephone contact').click();
+        await page.getByLabel('Telephone contact').fill('079965262');
+        await page.getByLabel('Alternate phone number (').click();
+        await page.getByLabel('Alternate phone number (').fill('0799659232');
+        await page.getByLabel('Contact Details Section').getByLabel('Postal Address (optional)').click();
+        await page.getByLabel('Contact Details Section').getByLabel('Postal Address (optional)').fill('9083');
+        await page.getByLabel('Email address (optional)').click();
+        await page.getByLabel('Email address (optional)').fill('brodu@gmail.com');
+        await page.getByLabel('Location', {exact: true}).click();
+        await page.getByLabel('Location', {exact: true}).fill('Timsd');
+        await page.getByLabel('Sub-location').click();
+        await page.getByLabel('Sub-location').fill('nyas');
+        await page.getByLabel('Village').click();
+        await page.getByLabel('Village').fill('mera');
+        await page.getByLabel('Landmark').click();
+        await page.getByLabel('Landmark').fill('uis');
+        await page.getByLabel('Marital status').selectOption('5555AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+        await page.getByLabel('Occupation').selectOption('1539AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+        await page.getByLabel('Education').selectOption('1714AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+        await page.getByLabel('Name (optional)', {exact: true}).click();
+        await page.getByLabel('Name (optional)', {exact: true}).fill('mike');
+        await page.getByLabel('Relationship (optional)').click();
+        await page.getByLabel('Relationship (optional)').fill('wifue');
+        await page.getByLabel('Phone Number (optional)', {exact: true}).click();
+        await page.getByLabel('Phone Number (optional)', {exact: true}).fill('0712345678');
+        await page.getByLabel('Next of Kin Details Section').getByLabel('Postal Address (optional)').click();
+        await page.getByLabel('Next of Kin Details Section').getByLabel('Postal Address (optional)').fill('8929');
+        await page.getByRole('button', {name: 'Post to registry'}).click();
+        await page.getByLabel('Telephone contact').click();
+        await page.getByLabel('Telephone contact').fill('0799652623');
+        await page.getByRole('button', {name: 'Register Patient'}).click();
+        await page.getByLabel('Nearest Health Center').fill('rtt');
+        await page.getByRole('button', {name: 'Register Patient'}).click();
+    });
+    await test.step('Then I should see a success notification', async () => {
+        await expect(page.getByText(/new patient created/i)).toBeVisible();
+    });
 
-  await test.step("And I click the `No` tab in the `Patient's Name is Known?` field", async () => {
-    await page.getByRole('tab', { name: /no/i }).first().click();
-  });
+    await test.step("And I should be redirected to the new patient's chart page", async () => {
+        const patientChartUrlRegex = new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$');
+        await page.waitForURL(patientChartUrlRegex);
+        await expect(page).toHaveURL(patientChartUrlRegex);
 
-  await test.step('And then I set the gender to `Female`', async () => {
-    await page
-      .locator('label')
-      .filter({ hasText: /female/i })
-      .locator('span')
-      .first()
-      .click();
-  });
+    });
 
-  await test.step('And then I click on the `No` tab in the "Date of Birth Known" field', async () => {
-    await page.getByRole('tab', { name: /no/i }).nth(1).click();
-  });
-
-  await test.step('And then I fill in 25 as the estimated age in years', async () => {
-    const estimatedAgeField = await page.getByLabel(/estimated age in years/i);
-    await estimatedAgeField.clear();
-    await estimatedAgeField.fill('25');
-  });
-
-  await test.step('And I click on the submit button', async () => {
-    await page.getByRole('button', { name: /register patient/i }).click();
-  });
-
-  await test.step('Then I should see a success notification', async () => {
-    await expect(page.getByText(/new patient created/i)).toBeVisible();
-  });
-
-  await test.step("And I should be redirected to the new patient's chart page", async () => {
-    const patientChartUrlRegex = new RegExp('^[\\w\\d:\\/.-]+\\/patient\\/[\\w\\d-]+\\/chart\\/.*$');
-    await page.waitForURL(patientChartUrlRegex);
-    await expect(page).toHaveURL(patientChartUrlRegex);
-  });
-
-  await test.step("And I should see the newly registered patient's details displayed in the patient banner", async () => {
-    const patientBanner = page.locator('header[aria-label="patient banner"]');
-
-    expect(patientBanner).toBeVisible();
-    await expect(patientBanner.getByText('Unknown Unknown')).toBeVisible();
-    await expect(patientBanner.getByText(/female/i)).toBeVisible();
-    await expect(patientBanner.getByText(/25 yrs/i)).toBeVisible();
-    await expect(patientBanner.getByText(/01 — Jan — 1999/i)).toBeVisible();
-    await expect(patientBanner.getByText(/OpenMRS ID/i)).toBeVisible();
-  });
+    await test.step("And I should see the newly registered patient's details displayed in the patient banner", async () => {
+        const patientBanner = page.locator('header[aria-label="patient banner"]');
+        await expect(patientBanner).toBeVisible();
+        await expect(patientBanner.getByText('Unknown Unknown')).toBeVisible();
+        await expect(patientBanner.getByText(/female/i)).toBeVisible();
+        await expect(patientBanner.getByText(/24 yrs/i)).toBeVisible();
+        await expect(patientBanner.getByText(/01 — Jan — 1999/i)).toBeVisible();
+        await expect(patientBanner.getByText(/OpenMRS ID/i)).toBeVisible();
+    });
 });
-
-test.afterEach(async ({ api }) => {
-  await deletePatient(api, patientUuid);
+test.afterEach(async ({api}) => {
+    await deletePatient(api, patientUuid);
 });
