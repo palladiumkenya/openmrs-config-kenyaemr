adad4ff7742233c31d37a89eff536bd979702ac3
Joshua Murithi <brodiamyzalius@gmail.com>
2024-11-19 06:08:36 +0300

    (tests): Kill a patient test
    Signed-off-by: Joshua Murithi <brodiamyzalius@gmail.com>

--- e2e/tests/specs/search/search-test.spec.ts
+++ e2e/tests/specs/search/search-test.spec.ts
@@ -7,7 +7,6 @@ import {HomePage} from "../../pages";
 let name = "James";
 test(`Searching client with  name \`${name}`, async ({page}) => {
     const home = new HomePage(page);
-
     await test.step("Once navigates to homepage", async () => {
         await home.gotoHome();
         await expect(page).toHaveURL(`${process.env.E2E_BASE_URL}/spa/home`);
@@ -16,20 +15,10 @@ test(`Searching client with  name \`${name}`, async ({page}) => {
     await test.step("Search clients by name", async ()=>{
         await page.getByTestId('searchPatientIcon').click();
         await page.getByTestId('patientSearchBar').fill(`${name}`);
+        await page.getByTestId('patientSearchBar').press('Enter');
+        await expect(page.getByRole('heading', { name: 'search results' })).toBeVisible();
 
     })
 
     }
 );
-
-
-// test('test', async ({ page }) => {
-//     await page.goto('https://qa.kenyahmis.org/openmrs/spa/login');
-//     await page.getByLabel('Username').click();
-//     await page.getByLabel('Username').fill('admin');
-//     await page.getByLabel('Username').press('Enter');
-//     await page.getByLabel('Password').fill('Admin123');
-//     await page.getByLabel('Password').press('Enter');
-//     await page.getByTestId('searchPatientIcon').click();
-//     await page.getByTestId('patientSearchBar').fill('test');
-// });
