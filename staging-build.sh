952e22fdd5ca53bd5a9536f038c5988a248165ff
Makombe Kennedy <kennedymakombe@gmail.com>
2025-01-24 17:10:00 +0300

    Rename patient tests dist folder (#1012)

--- staging-build.sh
+++ staging-build.sh
@@ -76,5 +76,6 @@ rename_dist_folder() {
 
 # Handle renaming for openmrs-esm-form-entry-app-*
 rename_dist_folder "openmrs-esm-form-entry-app-*" "dist-form-entry"
+rename_dist_folder "openmrs-esm-patient-tests-app-*" "dist-patient-tests"
 # Exit with success status
 exit 0
