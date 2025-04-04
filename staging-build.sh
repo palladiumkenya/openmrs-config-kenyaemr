bef348e8bcbce1dd95e6e943907f6c0575dffbd6
Donald Kibet <chelashawdonald@yahoo.com>
2025-04-04 14:07:22 +0300

    (chore) update openmrs version for staging (#1121)

--- staging-build.sh
+++ staging-build.sh
@@ -11,7 +11,7 @@ read -p "Is this for KDOD asset generation? (y/n): " is_kdod
 # Build assets
 echo "Building Kenya EMR 3.x assets ..."
 CWD=$(pwd)
-npx --legacy-peer-deps openmrs@6.0.3-pre.2654 build \
+npx --legacy-peer-deps openmrs@next build \
   --build-config ./frontend-config/staging/build-config.json \
   --target ./frontend \
   --page-title "KenyaEMR" \
@@ -19,7 +19,7 @@ npx --legacy-peer-deps openmrs@6.0.3-pre.2654 build \
 
 # Assemble assets
 echo "Assembling assets ..."
-npx --legacy-peer-deps openmrs@6.0.3-pre.2654 assemble \
+npx --legacy-peer-deps openmrs@next assemble \
   --manifest \
   --mode config \
   --config ./frontend-config/staging/build-config.json \
