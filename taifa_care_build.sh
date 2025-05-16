8d4c4a67f980d420403650aa0d0823f47fd55e6e
Donald Kibet <chelashawdonald@yahoo.com>
2025-05-16 14:33:32 +0300

    (fix) : fix error cause by core update

--- taifa_care_build.sh
+++ taifa_care_build.sh
@@ -11,7 +11,7 @@ read -p "Is this for KDOD asset generation? (y/n): " is_kdod
 # Build assets
 echo "Building Taifa Care KenyaEMR 3.x assets ..."
 CWD=$(pwd)
-npx --legacy-peer-deps openmrs@6.3.1-pre.2961 build \
+npx --legacy-peer-deps openmrs@6.3.1-pre.2951 build \
   --build-config ./frontend-config/staging/hie_build-config.json \
   --target ./frontend \
   --page-title "Taifa Care - KenyaEMR" \
@@ -19,7 +19,7 @@ npx --legacy-peer-deps openmrs@6.3.1-pre.2961 build \
 
 # Assemble assets
 echo "Assembling assets ..."
-npx --legacy-peer-deps openmrs@6.3.1-pre.2961 assemble \
+npx --legacy-peer-deps openmrs@6.3.1-pre.2951 assemble \
   --manifest \
   --mode config \
   --config ./frontend-config/staging/hie_build-config.json \
