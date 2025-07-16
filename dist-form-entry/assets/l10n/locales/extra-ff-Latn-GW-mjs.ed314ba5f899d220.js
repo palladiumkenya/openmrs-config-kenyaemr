dd8177e41fe343d081556f53d05c0e54b85dffde
Donald Kibet <chelashawdonald@yahoo.com>
2025-07-16 11:41:45 +0300

    feat: KHP3-8303 Enhance community admission request to be availed in clinical forms (#1381)

--- dist-form-entry/assets/l10n/locales/extra-ff-Latn-GW-mjs.ed314ba5f899d220.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_openmrs_esm_form_entry_app=self.webpackChunk_openmrs_esm_form_entry_app||[]).push([[83543],{91920:(s,_,e)=>{e.r(_),e.d(_,{default:()=>n});const n=[]}}]);
